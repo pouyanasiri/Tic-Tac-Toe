4d1ac7c2a34a4a8bc1583a1756f559f9de0bf045
pouyanasiri <pouyanasiri79@gmail.com>
2021-09-04 08:47:23 +0430

    add display function

---
+++ TicToc.py
@@ -0,0 +1,14 @@
+def display_board(board):
+    
+    clear_output()
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
